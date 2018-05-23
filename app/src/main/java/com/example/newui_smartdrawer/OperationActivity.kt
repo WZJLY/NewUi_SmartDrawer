669b880ad9730a805aea63b38c5f374f225f5936
Mazinglabs <243278397@qq.com>
2018-05-23 16:16:53 +0800

    UI

--- app/src/main/java/com/example/newui_smartdrawer/OperationActivity.kt
+++ app/src/main/java/com/example/newui_smartdrawer/OperationActivity.kt
@@ -2,11 +2,14 @@ package com.example.newui_smartdrawer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_operation.*
 
 class OperationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operation)
+
+        btn_operation_into.isEnabled = false
     }
 }
