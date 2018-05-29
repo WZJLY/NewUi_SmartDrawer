50bd7923d23325d87172610c1d1862502a119d10
Mazinglabs <243278397@qq.com>
2018-05-29 09:57:09 +0800

    UI：操作记录

---
+++ app/src/main/java/com/example/newui_smartdrawer/RecordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newui_smartdrawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
