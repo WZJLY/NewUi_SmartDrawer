e14c0579cd572786181c8ae7a2255921bb1fe7f0
Mazinglabs <243278397@qq.com>
2018-05-29 15:57:58 +0800

    table

--- app/src/main/java/com/example/newui_smartdrawer/TableFragment.kt
+++ app/src/main/java/com/example/newui_smartdrawer/TableFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableRow
 import kotlinx.android.synthetic.main.fragment_table.*
 
 
@@ -15,6 +17,32 @@ class TableFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_table, container, false)
+    }
 
+    fun addNum(num: Int){
+        tl_Ftable.removeAllViews()
+        val params = TableRow.LayoutParams(55, 55)
+        for(i in 1..num){
+            val tableRow = TableRow(context)
+            for(j in 1..num){
+                val button = Button(context)
+                button.layoutParams = params
+                button.isFocusable = false
+                button.id = (i-1)*num+j
+                button.textSize = 12F
+                //按钮底色和字体颜色
+                button.setBackgroundResource(R.drawable.btn_table_f6f6f6_style)
+                button.setTextColor(context.resources.getColor(R.color.tb_text))
+
+                button.setOnClickListener { view->
+                    view.isFocusable = true
+                    view.requestFocus()
+                    view.requestFocusFromTouch()
+                    var row = button.id.toString()
+                }
+                tableRow.addView(button)
+            }
+            tl_Ftable.addView(tableRow)
+        }
     }
 }
